8daa97affdc8e9ae4310c3b7d9179e139903ec27
Ayush Ahirwar <ayushahirwar6673@gmail.com>
2024-11-08 14:15:00 +0530

    route setup

---
+++ src/utils/Routing.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from '../components/Login'
+import Register from '../components/Register'
+import Forget from '../components/Forget'
+import Home from '../components/Home'
+
+const Routing = () => {
+  return (
+    <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/forget" element={<Forget/>} />
+        <Route path='/' element={<Home/>}></Route>
+  
+    </Routes>
+  )
+}
+
+export default Routing
